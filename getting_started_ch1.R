dbef3881c535467e86cfde8eccb3f71c1b5bd6c5
howarddanielchen <howard1980chen@gmail.com>
2026-01-22 13:33:02 -0600

    feat(file): chapter 1 complete

---
+++ getting_started_ch1.R
@@ -0,0 +1,13 @@
+#* This is the practice file for chapter 1 from Muroch's R for Data Analysis
+#* 
+# Creates a variable
+message <- "Hello World"
+
+# Displays the value in the variable
+message
+
+# Create two two varables with values and a variable to calculate the average
+total <- 453
+count <- 32
+avg <- total / count
+avg
